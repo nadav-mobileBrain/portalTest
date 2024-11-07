2b3d3c5e6148a92a9309b59b1979da9aebb552e1
nadav-mobileBrain <nadavg@mobile-brain.net>
2024-11-07 13:50:48 +0200

    ignore typescript

--- app/@page.tsx
+++ app/@page.tsx
@@ -1,1 +1,12 @@
- 
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
+export default function Page() {
+  const searchParams = useSearchParams();
+  const wrongAnswers = searchParams.get("wrongAnswers") || "0";
+
+  return (
+    <div className="text-lg font-semibold">Wrong Answers: {wrongAnswers}</div>
+  );
+}
